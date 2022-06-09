af99efaf59f90f8b9539a75132a13f74b87dc8ee
AlexSandlerG <alex-sandler_java@mail.ru>
2022-06-10 01:57:34 +0300

    1.7. Конструктор [#173889 #42120]

---
+++ src/main/java/oop/Error.java
@@ -0,0 +1,32 @@
+package oop;
+
+public class Error {
+
+    String message;
+    boolean active;
+    int status;
+
+    public Error() {
+
+    }
+
+    public Error(String message, boolean active, int status) {
+        this.message = message;
+        this.active = active;
+        this.status = status;
+    }
+
+    public void show() {
+        System.out.println(this.message);
+        System.out.println(this.active);
+        System.out.println(this.status);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.show();
+        System.out.println("");
+        Error error1 = new Error("text", true, 5);
+        error1.show();
+    }
+}
