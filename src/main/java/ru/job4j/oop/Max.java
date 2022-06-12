e17b9b09ea0ec74c7db2aa49ce17bbb3595003b2
AlexSandlerG <alex-sandler_java@mail.ru>
2022-06-12 19:17:53 +0300

    1.9. Статические и не статические методы [#504806 #238226]

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -10,6 +10,7 @@ public class Max {
         int tmp = max(one, two);
         return max(tmp, three);
     }
+
     public int max(int one, int two, int three, int four) {
         int tmp = max(one, two);
         return max(tmp, max(three, four));
