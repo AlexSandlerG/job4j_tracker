6d0faf76b26d75bd309da5010d98121f23583974
AlexSandlerG <alex-sandler_java@mail.ru>
2022-06-13 15:51:08 +0300

    1.9. Статические и не статические методы [#504806 #238226]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     private static int x = 5;
 
     public static int minus(int y) {
-        return x - y;
+        return y - x;
     }
 
     public int divide(int y) {
