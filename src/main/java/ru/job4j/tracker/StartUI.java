b14f7721356369e4f7713a60f15705dac4a86fde
AlexSandlerG <alex-sandler_java@mail.ru>
2022-06-26 19:31:30 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #316565]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -16,6 +16,7 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
@@ -26,6 +27,15 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+                System.out.println("Заявка изменена успешно.");
             } else if (select == 6) {
                 run = false;
             }
