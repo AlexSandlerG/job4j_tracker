eb5ff171d20d6774888845743637f77031f042b0
AlexSandlerG <alex-sandler_java@mail.ru>
2022-07-02 19:43:17 +0300

    3. Интерфейс Input [#181092 #42071]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void ride() {
+        System.out.println(getClass().getSimpleName() + "ездит по трасам");
+    }
+
+    @Override
+    public void passengers(int quantity) {
+        System.out.println(getClass().getSimpleName() + 20);
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return (fuel * 7) / 100;
+    }
+}
